c2e9efe1465312002fa50bb455e9145f0135bbfc
RichterBeau <54587515+RichterBeau@users.noreply.github.com>
2020-07-31 18:07:07 -0400

    Update obdtest.py

--- code/obdtest.py
+++ code/obdtest.py
@@ -1,5 +1,5 @@
 import obd
-import threading
+import smtplib
 from guizero import App,Text,Box
 
 obd.logger.setLevel(obd.logging.DEBUG)
@@ -7,18 +7,25 @@ ports =  obd.scan_serial()
 print (ports)
 connection = obd.OBD()
 
+#chk=0
+eFROM = "richterbeaua@gmail.com"
+eTO = "6316455069@vtext.com"
+Subject = "Speeding Alert"
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 
-def getCool():
-    cmd = obd.commands.COOLANT_TEMP
-    response = connection.query(cmd)
-    cool0 = str(response.value)
-    cool = cool0 +" *C"
-    coolText.clear()
-    coolText.append(cool)
-    coolext.after(1000, getCool)
+
+def speedAlert(): #currently not working, issue with defining chkbit
+    if chk == 0:
+        text = "Someone is going ph in your car!"
+        eMessage = 'Subject: {}\n\n{}'.format(Subject, text)
+        server.login("richterbeaua@gmail.com", "tyecavldtgopuspz")
+        server.sendmail(eFROM, eTO, eMessage)
+        chk = 1
+        
+
 
 def update():
-    cmd1 = obd.commands.SPEED
+    cmd1 = obd.commands.SPEED  
     cmd2 = obd.commands.RPM
     cmd3 = obd.commands.COOLANT_TEMP
     cmd4 = obd.commands.RUN_TIME
@@ -26,20 +33,23 @@ def update():
     response2 = connection.query(cmd2)
     response3 = connection.query(cmd3)
     response4 = connection.query(cmd4)
-    rpm0 = str(response2.value) #RPM
+    rpm0 = str(response2.value)             #RPM
     rpm = rpm0[0:5]
     rpmText.clear()
     rpmText.append(rpm)
     spd0 = str(response1.value.to("mph"))   #SPEED
+    #spdCompare = spd0[3]
+    #if spdCompare == ".": #if the 4th character of the string is . the speed is 3 digits
+          #speedAlert()
     spd = spd0[0:3]
     spdText.clear()
     spdText.append(spd)
-    cool0 = str(response3.value)  #COOLANT TEMP
-    cool = cool0 +" *C"
+    cool0 = str(response3.value)            #COOLANT TEMP
+    cool = cool0 
     coolText.clear()
     coolText.append(cool)
-    time0 = str(response4.value)   # RUN TIME
-    time = time0 +" *Seconds"
+    time0 = str(response4.value)            # RUN TIME
+    time = time0 
     runText.clear()
     runText.append(time)
     spdText.after(1, update)
@@ -51,17 +61,17 @@ rpmMeter = Text(app, text="    RPM  ", size=30, grid = [0,0])
 rpmBox = Box(app, width = 400, height = 80,grid = [0,1])
 rpmBox.set_border(4, "black")
 rpmText = Text(rpmBox, text = "0", size = 30, align = "bottom")
-#rpmText.after(1, getRPM)
+
 
 speedoMeter = Text(app, text="    Speed (mph)  ", size=30 ,grid = [1,0])
 spdBox = Box(app, width = 400, height = 80, grid = [1,1])
 spdBox.set_border(4, "black")
 spdText = Text(spdBox, text = "0", size = 30, align = "bottom")
-#spdText.after(1, getSpd)
+
 
 coolMeter = Text(app, text="Coolant Temp  = ", size=25 ,grid = [0,3])
 coolText = Text(app, text = "80 *C", size = 25, grid = [1,3])
-#coolText.after(1000, getTime)
+
 
 runTime = Text(app, text="Car Runtime  = ", size=25 ,grid = [0,4])
 runText = Text(app, text = "0 Seconds", size = 25, grid = [1,4])
